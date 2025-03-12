7760fa9edf37e8933dc6549aab8291259a8d61a9
JB4000 <jabi0002@stud.kea.dk>
2025-03-12 10:28:38 +0100

    clean main

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,7 @@ public class Main {
 
         run();
 
-        System.out.printf("Hello and welcome! o/");
 
-        for (int i = 1; i <= 55; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println("HEyyy");
-        }
 
 
     }
