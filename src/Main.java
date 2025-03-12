0fb56ed401bb522e2a9f8087aed79bb25f98fa49
Raged213 <raal004@stud.kea.dk>
2025-03-12 10:49:09 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
+        welcomeMessage();
         run();
-
-
-
+        coffeeChoice();
 
     }
+
+    //Line Breaker
+    public static void lineBreaker(){
+        System.out.println("-------------------------------------------------");
+    }
+
+    //Welcome message!
+    public static void welcomeMessage(){
+        System.out.println("Hey and welcome to our Barista!" +
+                "\nHere can you order coffee and Choko Milk");
+        lineBreaker();
+    }
+
+    //Ask the user what they want
+
+    public static void coffeeChoice(){
+        System.out.println("Here are the option we have\n" +
+                "1: Black Coffee\n" +
+                "2: Espresso\n" +
+                "3: Dobbelt espresso \n" +
+                "4: Triple espresso \n" +
+                "5: Latte Machiatto \n" +
+                "6: Kakaomælk (kan tilføjes flødeskum) \n" +
+                "7: Chokoladedrik (kan tilføjes flødeskum) \n");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1 - 7 ");
+
+    }
+
+
+
 
     public static void run() {
 
