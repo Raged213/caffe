5430159fa130a47576fcac0f7214b7f330c6993c
JVRKEA <juri0002@stud.kea.dk>
2025-03-12 10:35:06 +0100

    choco

--- src/Choco.java
+++ src/Choco.java
@@ -1,2 +1,14 @@
 public class Choco {
+private String skum;
+
+public String getSkum() {
+return skum;
+}
+public void setSkum(String skum) {
+this.skum = skum;
+}
+
+public Choco(String skum){
+this.skum = skum;
+}
 }
